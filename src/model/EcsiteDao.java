50badc234459cd2f0557a0ad59c04e640d381f2c
竹原 <takehara@gmail>
2017-12-06 12:22:50 +0900

    dao途中ですが

--- src/model/EcsiteDao.java
+++ src/model/EcsiteDao.java
@@ -8,7 +8,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import database.Category_tblVo;
+import database.Hard_tblVo;
+import database.Product_mstVo;
+import database.Product_pic_tblVo;
+import database.Recommend_tblVo;
 
+/***
+ * Dao
+ * @author takehara
+ *
+ */
 public class EcsiteDao implements AutoCloseable {
 
     protected Connection connection;
@@ -35,6 +44,11 @@ public class EcsiteDao implements AutoCloseable {
         }
     }
 
+    /***
+     * カテゴリーの取得
+     * @return
+     * @throws SQLException
+     */
     public ArrayList<Category_tblVo> getCategoryList() throws
             SQLException {
         System.out.println("\n/// getCategoryList()");
@@ -56,4 +70,134 @@ public class EcsiteDao implements AutoCloseable {
         }
         return entList;
     }
+
+    /***
+     * ハードの取得
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Hard_tblVo> getHardList() throws
+            SQLException {
+        System.out.println("\n/// getHardList()");
+
+        String sql =
+                "SELECT * FROM hard_tbl" +
+                        " ORDER BY hard_id";
+
+        ArrayList<Hard_tblVo> entList = new ArrayList<Hard_tblVo>();
+        try (PreparedStatement pstatement = connection.prepareStatement(sql)) {
+            System.out.println("--- sql = " + pstatement);
+            ResultSet rs = pstatement.executeQuery();
+            while (rs.next()) {
+                Hard_tblVo ent = new Hard_tblVo();
+                ent.setHard_id(rs.getInt("hard_id"));
+                ent.setHard_name(rs.getString("hard_name"));
+                entList.add(ent);
+            }
+        }
+        return entList;
+    }
+
+    /***
+     * おすすめ商品の取得
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Recommend_tblVo> getRecommendList() throws
+            SQLException {
+        System.out.println("\n/// getRecommendList()");
+
+        String sql =
+                "SELECT * FROM recommend_tbl";
+
+        ArrayList<Recommend_tblVo> entList = new ArrayList<Recommend_tblVo>();
+        try (PreparedStatement pstatement = connection.prepareStatement(sql)) {
+            System.out.println("--- sql = " + pstatement);
+            ResultSet rs = pstatement.executeQuery();
+            while (rs.next()) {
+                Recommend_tblVo ent = new Recommend_tblVo();
+                ent.setProduct_id(rs.getInt("product_id"));
+                ent.setRecommend_pic(rs.getString("recommend_pic"));
+                entList.add(ent);
+            }
+        }
+        return entList;
+    }
+
+    /***
+     * トップ画面用の商品IDとロゴ画像の取得
+     * @param hard_id
+     * @param category_id
+     * @param convention_word
+     * @return
+     * @throws SQLException
+     */
+    /*
+    public ArrayList<DtoTopProduct> getProductList(String hard_id, String category_id, String convention_word)
+            throws SQLException {
+        System.out.println("\n/// getProductList()");
+
+        return entList;
+    }
+    */
+
+    /***
+     * 指定した商品IDのレコードを商品マスタから取得
+     * @param product_id
+     * @return
+     * @throws SQLException
+     */
+    public Product_mstVo getProductmust(int product_id) throws
+            SQLException {
+        System.out.println("\n/// getProductmust()");
+
+        Product_mstVo ent = new Product_mstVo();
+        String sql = "SELECT * FROM product_must WHERE product_id=?";
+
+        try (PreparedStatement pstatement = connection.prepareStatement(sql)) {
+            pstatement.setInt(1, product_id);
+            System.out.println("--- sql = " + pstatement);
+            ResultSet rs = pstatement.executeQuery();
+            if (rs.next()) {
+                ent.setProduct_id(rs.getInt("product_id"));
+                ent.setProduct_name(rs.getString("product_name"));
+                ent.setPrice(rs.getInt("price"));
+                ent.setStocks(rs.getInt("stocks"));
+                ent.setComment(rs.getString("comment"));
+                ent.setHard_id(rs.getInt("hard_id"));
+                ent.setCategory_id(rs.getInt("category_id"));
+            }
+        }
+        return ent;
+    }
+
+    /***
+     * 商品詳細画面の画像取得※ロゴまで取得している※要修正※
+     * @param product_id
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Product_pic_tblVo> getPictureList(int product_id) throws
+            SQLException {
+        System.out.println("\n/// getPIctureList()");
+
+        String sql = "SELECT * product pic_tbl from product_id=?";
+
+        ArrayList<Product_pic_tblVo> entList = new ArrayList<Product_pic_tblVo>();
+        try (PreparedStatement pstatement = connection.prepareStatement(sql)) {
+            pstatement.setInt(1, product_id);
+            System.out.println("--- sql = " + pstatement);
+            ResultSet rs = pstatement.executeQuery();
+            while (rs.next()) {
+                Product_pic_tblVo ent = new Product_pic_tblVo();
+                ent.setProduct_id(rs.getInt("product_id"));
+                ent.setPic_category(rs.getInt("pic_category"));
+                ent.setPic_number(rs.getInt("pic_number"));
+                ent.setPic_file(rs.getString("pic_file"));
+                entList.add(ent);
+            }
+        }
+        return entList;
+    }
+
 }
--- src/model/EcsiteDaoTest.java
+++ src/model/EcsiteDaoTest.java
@@ -7,15 +7,45 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 import database.Category_tblVo;
+import database.Hard_tblVo;
+import database.Recommend_tblVo;
 
 public class EcsiteDaoTest {
 
     @Test
-    public void test() {
+    public void testGetCategoryList() {
         try (EcsiteDao dao = new EcsiteDao()) {
             ArrayList<Category_tblVo> entList;
             entList = dao.getCategoryList();
             for (Category_tblVo ent : entList) {
+                System.out.println(ent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetHardList() {
+        try (EcsiteDao dao = new EcsiteDao()) {
+            ArrayList<Hard_tblVo> entList;
+            entList = dao.getHardList();
+            for (Hard_tblVo ent : entList) {
+                System.out.println(ent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetRecommendList() {
+        try (EcsiteDao dao = new EcsiteDao()) {
+            ArrayList<Recommend_tblVo> entList;
+            entList = dao.getRecommendList();
+            for (Recommend_tblVo ent : entList) {
                 System.out.println(ent);
             }
         } catch (Exception e) {
